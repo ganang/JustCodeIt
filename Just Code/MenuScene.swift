6e41607a11d083d994c1eebd8f1875d08ebfc5f5
Ganang <ganang.arief.p@gmail.com>
2020-04-13 17:52:15 +0700

    first commit

---
+++ Just Code/MenuScene.swift
@@ -0,0 +1,13 @@
+//
+//  MenuScene.swift
+//  Just Code
+//
+//  Created by Ganang Arief Pratama on 10/04/20.
+//  Copyright © 2020 CodeFun. All rights reserved.
+//
+
+import UIKit
+
+class MenuScene: SKScene {
+
+}
---
+++ Just Code/Scenes/VariablesGame/VariablesGameScene.swift
@@ -0,0 +1,15 @@
+//
+//  VariablesGameScene.swift
+//  Just Code
+//
+//  Created by Ganang Arief Pratama on 10/04/20.
+//  Copyright © 2020 CodeFun. All rights reserved.
+//
+
+import SpriteKit
+
+class VariablesGameScene: SKScene {
+    
+    
+
+}
